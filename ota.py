d9c44f3c16ee582a34bb4f7f80131b52b51bcb28
rumplestilzken <rumplestilzken@gmail.com>
2023-09-06 05:48:57 -0400

    Fix capital N

--- ota.py
+++ ota.py
@@ -9,20 +9,20 @@ def get_variant_map(qualifier):
     if "vndk" in qualifier:
         dict = {
             "Titan": "gargoyle_bvN-vndklite",
-            "Titan bgn": "gargoyle_pocket_bgn-vndklite",
+            "Titan bgn": "gargoyle_pocket_bgN-vndklite",
         }
     elif "bgN" in qualifier:
         dict = {
             "Titan": "gargoyle_bgN",
             "Titan Pocket": "gargoyle_pocket_bgN",
             "Titan Slim": "gargoyle_slim_bgN",
-            "Jelly 2E": "gargoyle_jelly2e_bgn",
+            "Jelly 2E": "gargoyle_jelly2e_bgN",
         }
     else:
         dict = {
             "Titan": "gargoyle_bvN",
             "Titan Pocket": "gargoyle_pocket_bvN",
             "Titan Slim": "gargoyle_slim_bvN",
-            "Jelly 2E": "gargoyle_jelly2e_bvn",
+            "Jelly 2E": "gargoyle_jelly2e_bvN",
         }
     return dict
